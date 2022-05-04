efecd49fa6142ebcee0023f403d40866f6ff2d26
xyz2b <you@example.com>
2022-05-04 10:56:59 +0800

    init

---
+++ share/Object.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xyzjiao on 5/3/22.
+//
+
+#include "Object.h"
---
+++ share/Object.h
@@ -0,0 +1,14 @@
+//
+// Created by xyzjiao on 5/3/22.
+//
+
+#ifndef GC_OBJECT_H
+#define GC_OBJECT_H
+
+
+class Object {
+
+};
+
+
+#endif //GC_OBJECT_H
